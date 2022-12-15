012209c3ece9f22e93f3dfafd3512056f205c9a0
danhillida <dan.hill.ida@gmail.com>
2022-12-14 19:34:28 -0500

    Complete Exercise

--- Javascript-Functions/app.js
+++ Javascript-Functions/app.js
@@ -1,6 +1,33 @@
 "use strict"
 
+/*
 const list = ['tiger', 'cat', 'bear', 'bird'];
 console.log(list[1]);
 
 console.log(list);
+
+// EXERCISE 6
+
+const array = ["Banana", "Apples", "Oranges", 'Blueberries'];
+
+array.shift();
+array.sort();
+console.log(array.push('Kiwi'));
+array.shift();
+array.reverse();
+console.log(array);
+
+const array2 = ['Banana', ['Apples', ['Oranges'], 'Blueberries']];
+
+console.log(array2[1][1][0]);
+*/
+
+const user = {
+    name: 'John',
+    age: 34,
+    hobby: 'Soccer',
+    isMarried: false,
+}
+
+console.log(user);
+
