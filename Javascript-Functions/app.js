ed3397a156d3162afa01c25691b7142e750662e8
danhillida <dan.hill.ida@gmail.com>
2022-12-15 19:44:31 -0500

    Practiced login functionality

--- Javascript-Functions/app.js
+++ Javascript-Functions/app.js
@@ -20,7 +20,7 @@ console.log(array);
 const array2 = ['Banana', ['Apples', ['Oranges'], 'Blueberries']];
 
 console.log(array2[1][1][0]);
-*/
+
 
 const user = {
     name: 'John',
@@ -54,3 +54,62 @@ console.log(user);
 
 //A Function inside an object is called a "method"
 console.log(user.shout());
+
+
+const duckFart = {
+    userName: 'duckfart',
+    password: 'chickenqueef'
+}
+
+const database = [duckFart]
+
+console.log(database[0].userName)
+
+const newsfeed = [
+    {
+        username: 'buckslayer',
+        timeline: ''
+    },
+
+    {
+        username: 'hardkordann',
+        timeline: ''
+    },
+
+    {
+        username: 'kmae222',
+        timeline: ''
+    }
+]
+*/
+
+const database = [
+    {
+        username: 'andrei',
+        password: 'supersecret'
+    }
+];
+
+const newsfeed = [
+    {
+        username: 'Bobby',
+        timeline: 'So tired from all that learning'
+    },
+    {
+        username: 'Sally',
+        timeline: 'Javascript is sooooooo cool!'
+    }
+];
+
+const userNamePrompt = prompt("What's your username?");
+const passwordPrompt = prompt("What's your password?");
+
+function signIn(username, pass) {
+    if (username === database[0].username && pass === database[0].password) {
+        console.log(newsfeed);
+    } else {
+        alert('Sorry, the username or password were incorrect');
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt)
