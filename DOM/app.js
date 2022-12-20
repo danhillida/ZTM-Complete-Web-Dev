4f491a7081a3d3c0e426a7858d3088a6c0dd13dd
danhillida <dan.hill.ida@gmail.com>
2022-12-20 09:05:08 -0500

    Woking on DOM

---
+++ DOM/app.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// ----- Elements -----
+const button = document.getElementById('enter');
+const input = document.getElementById('userinput');
+const ul = document.querySelector('ul');
+
+button.addEventListener('click', function() {
+    if (input.value.length > 0) {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = '';
+    }
+})
