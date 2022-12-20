32f8d93856f127ab7420af6106f401756b0b8de4
danhillida <dan.hill.ida@gmail.com>
2022-12-20 14:04:17 -0500

    displayed the css to h3

--- background-generator/app.js
+++ background-generator/app.js
@@ -7,8 +7,8 @@ console.log(body);
 
 const setGradient = function () {
     body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener('input', setGradient);
-
 color2.addEventListener('input', setGradient);
